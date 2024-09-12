4442edcd2f5780f1dedbfac72d78c429f6f54785
Melissa Montesinos <melissa.d.montesinos@gmail.com>
2024-09-12 00:07:05 -0400

    "Initialize inventory with product objects"

---
+++ inventory_management.js
@@ -0,0 +1,10 @@
+
+// Task 1: Create an Inventory Array of Product Objects
+
+let inventory = [
+    { name:"Mattress", price: 522, quantity: 138, lowStockLevel: 22},
+    { name: "Pillow", price: 25, quantity: 212, lowStockLevel: 18},
+    { name: "Comforter", price: 121, quantity: 98, lowStockLevel: 14},
+    { name: "Nightstand", price: 89, quantity: 66, lowStockLevel: 8}, 
+    { name: "Headboards", price: 102, quantity: 78, lowStockLevel: 9},
+];
